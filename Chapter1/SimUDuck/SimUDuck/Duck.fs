5ae352026a1f26edf905b584e337e4b4db67b8cf
Kevin Pluck <kevinjpluck@gmail.com>
2015-10-25 11:16:27 +0000

    Chapter one added. The Strategy Pattern

---
+++ Chapter1/SimUDuck/SimUDuck/Duck.fs
@@ -0,0 +1,16 @@
+﻿namespace SimUDuck
+module Duck =
+
+    type Duck = 
+        | Mallard
+        | RubberDuck
+        | Decoy
+
+    let quack = function
+        | Mallard -> printfn "QUACK"
+        | RubberDuck -> printfn "SQUEAK"
+        | Decoy -> printfn "<silence>"
+
+    let fly = function
+        | Mallard -> printfn "I can fly!"
+        | RubberDuck| Decoy -> printfn "I can't fly"
---
+++ Chapter1/SimUDuck/SimUDuck/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open SimUDuck.Duck
+
+[<EntryPoint>]
+let main argv = 
+    
+
+    quack Mallard
+    quack Decoy
+
+    fly Mallard
+    fly Decoy
+    fly RubberDuck
+    0
